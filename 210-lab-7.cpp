e54e3f39da083ea9ff94ad892385c34e09394799
gillianrhett <gillianrhett@gmail.com>
2026-02-15 14:48:53 -0800

    tested functions with ints

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -4,15 +4,30 @@
 using namespace std;
 
 string* reverseArray(string*, int);
+int* reverseIntArray(int*, int); // TESTING
 void displayArray(string*, int);
+void displayIntArray(int*, int); // TESTING
 
-const int arrSize = 5;
+const int arrSize = 6;
 
 int main() {
     // create a dynamic array of strings
     // problem: the size of strings varies
     // I could use a vector?
 
+    // TESTING
+    int* intArr = new int {arrSize};
+    *(intArr + 0) = 10;
+    *(intArr + 1) = 20;
+    *(intArr + 2) = 30;
+    *(intArr + 3) = 40;
+    *(intArr + 4) = 50;
+    *(intArr + 5) = 60;
+    cout << "Original array: ";
+    displayIntArray(intArr, arrSize);
+    cout << "Reversed array: ";
+    displayIntArray(reverseIntArray(intArr, arrSize), arrSize);
+    
     return 0;
 }
 
@@ -23,12 +38,37 @@ string* reverseArray(string* arr, int size) {
     ptr = arr;
     string temp;
     for(int i = 0; i < size / 2; ++i) {
-        // TODO finish this
+        temp = *(ptr + i);
+        *(ptr + i) = *(ptr + ((size - i) - 1));
+        *(ptr + ((size - i) - 1)) = temp;
     }
     return ptr;
 }
 
 void displayArray(string* arr, int size) {
+// show the array elements in order on one line
+    for(int i = 0; i < size; ++i) {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+}
+
+// TESTING making sure my functions work
+int* reverseIntArray(int* arr, int size) {
+// reverse the elements in the given array
+//  and return a pointer to this reversed array
+    int* ptr;
+    ptr = arr;
+    int temp;
+    for(int i = 0; i < size / 2; ++i) {
+        temp = *(ptr + i);
+        *(ptr + i) = *(ptr + ((size - i) - 1));
+        *(ptr + ((size - i) - 1)) = temp;
+    }
+    return ptr;
+}
+
+void displayIntArray(int* arr, int size) {
 // show the array elements in order on one line
     for(int i = 0; i < size; ++i) {
         cout << *(arr + i) << " ";
