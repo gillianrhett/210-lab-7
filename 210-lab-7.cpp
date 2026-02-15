da14c32d5d0133edc9e4d8c5f4bab1805aa3e08e
gillianrhett <gillianrhett@gmail.com>
2026-02-15 15:26:17 -0800

    working on it with vectors

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -17,31 +17,32 @@ int main() {
     
     // trying it with a vector
     vector<string> myStrs {"alpha", "beta", "gamma", "delta", "epsilon"};
-
-
-    /*
     cout << "Original array: ";
-    displayArray(strArr, arrSize);
+    displayArray(myStrs, arrSize);
+    vector<string>* myStrsPtr = reverseArray(myStrs, arrSize);
     cout << "Reversed array: ";
-    displayArray(reverseArray(strArr, arrSize), arrSize);
-    
-    delete strArr;    
-    strArr = nullptr;
-    */
+    displayArray(myStrs, arrSize);
+
     return 0;
 }
 
 vector<string>* reverseArray(vector<string> vectIn, int size) {
+// receives a dynamic string array, reverses the elements,
+//  and returns a pointer to the modified array (which is the same array??)
     vector<string>* ptr = &vectIn;
     string temp;
-    for(int i = 0; i < arrSize; ++i) {
-
+    for(int i = 0; i < arrSize / 2; ++i) {
+        temp = vectIn.at(i);
+        vectIn.at(i) = vectIn.at((size - i) - 1);
+        vectIn.at((size - i) - 1) = temp;
     }
     return ptr;
 }
 
-void displayArray(vector<string>, int) {
-
+void displayArray(vector<string> vectIn, int size) {
+    for(int i = 0; i < size; ++i)
+        cout << vectIn.at(i) << " ";
+    cout << endl;
 }
 
 // TESTING making sure my functions work
