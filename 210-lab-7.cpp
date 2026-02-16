da9eb0b7d3ffd484f5c1ebc01300fadc05caf4d9
gillianrhett <gillianrhett@gmail.com>
2026-02-15 16:00:01 -0800

    reverseArray() done

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -11,7 +11,21 @@ const int arrSize = 5;
 
 int main() {
 // create a dynamic array of strings and reverse it
+    
+    // create and populate the array
     string* strArr = new string[arrSize];
+    *(strArr + 0) = "alpha";
+    *(strArr + 1) = "beta";
+    *(strArr + 2) = "gamma";
+    *(strArr + 3) = "delta";
+    *(strArr + 4) = "epsilon";
+    
+    
+    string* revStrArr = reverseArray(strArr, arrSize);
+
+    // deallocate memory
+    delete[] strArr;
+    strArr = nullptr;
 
     return 0;
 }
@@ -20,9 +34,10 @@ string* reverseArray(string* arr, int size) {
 // creates a new array in reverse order from the given array
 //   and returns a pointer to the new array
 //   this new array will need to be deallocated by the caller
-    string* ptr = nullptr;
-
-    return ptr;
+    string* revArr = new string[size];
+    for(int i = 0; i < size; ++i)
+        *(revArr + i) = *(arr + ((size - i) - 1));
+    return revArr;
 }
 
 void displayArray(string* arr, int size) {
