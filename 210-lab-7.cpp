706fa82f6a70a7cae121700161e84fbfb493c9a3
gillianrhett <gillianrhett@gmail.com>
2026-02-15 16:03:38 -0800

    both functions work

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -20,12 +20,17 @@ int main() {
     *(strArr + 3) = "delta";
     *(strArr + 4) = "epsilon";
     
-    
+    cout << "Original array: ";
+    displayArray(strArr, arrSize);
     string* revStrArr = reverseArray(strArr, arrSize);
+    cout << "Reversed array: ";
+    displayArray(revStrArr, arrSize);
 
     // deallocate memory
     delete[] strArr;
     strArr = nullptr;
+    delete[] revStrArr;
+    revStrArr = nullptr;
 
     return 0;
 }
@@ -41,50 +46,7 @@ string* reverseArray(string* arr, int size) {
 }
 
 void displayArray(string* arr, int size) {
-
-}
-
-/*
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
-
-vector<string>* reverseArray(vector<string>, int);
-void displayArray(vector<string>);
-
-const int arrSize = 5;
-
-int main() {
-// create a dynamic array of strings and reverse it    
-    vector<string> myStrs {"alpha", "beta", "gamma", "delta", "epsilon"};
-    cout << "Original array: ";
-    displayArray(myStrs);
-    vector<string>* myStrsRev = reverseArray(myStrs, arrSize);
-    cout << "Reversed array: ";
-    displayArray(*myStrsRev);
-
-    delete myStrsRev;
-    myStrsRev = nullptr;
-
-    return 0;
-}
-
-vector<string>* reverseArray(vector<string> vectIn, int size) {
-// receives a dynamic string array, reverses the elements,
-    vector<string>* revVectPtr = new vector<string>;
-    // necessary in order for the reversed vector to still exist after function ends
-    // caller will have to deallocate it
-
-    for(int i = 0; i < arrSize; ++i)
-        (*revVectPtr).push_back(vectIn.at((size - i) - 1));
-    return revVectPtr;
-}
-
-void displayArray(vector<string> vectIn) {
-// display the elements from the vector on one line
-    for(string s : vectIn)
-        cout << s << " ";
+    for(int i = 0; i < size; ++i)
+        cout << *(arr + i) << " ";
     cout << endl;
 }
-*/
